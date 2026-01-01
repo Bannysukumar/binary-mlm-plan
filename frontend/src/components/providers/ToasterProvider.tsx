e00897fa598499e7c8847368c6b2cce861ef9811
munna <munnacys@gmail.com>
2026-01-01 14:09:22 +0530

    Remove unused @ts-expect-error directive in ToasterProvider

--- frontend/src/components/providers/ToasterProvider.tsx
+++ frontend/src/components/providers/ToasterProvider.tsx
@@ -3,6 +3,5 @@
 import { Toaster } from 'react-hot-toast';
 
 export function ToasterProvider() {
-  // @ts-expect-error - react-hot-toast type compatibility issue with React 18/Next.js 14
   return <Toaster position="top-right" />;
 }
